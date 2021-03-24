e135e762084fbbdfc3e9b00ca984b24f37650ac4
DerikCaceres <derik.maganhato@hotmail.com>
2021-03-24 08:32:44 -0300

    Criação banco teste

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_CLIENT (nome,lugar) VALUES ('Carlos', 'Rua Barao, 456 – Sorocaba/SP');
+INSERT INTO tb_CLIENT (nome,lugar) VALUES ('Nereu', 'Rua Augusta, 199 – Sorocaba/SP');
+INSERT INTO tb_CLIENT (nome,lugar,email) VALUES ('Joana', 'Avenida das figueiras – Tatui/SP','Jo.ana@hotmail.com');
+INSERT INTO tb_CLIENT (nome,lugar,email) VALUES ('Maria ', 'Rodovia joao leme, 235 – Sao Paulo/SP','maria.m@gmail.com');
