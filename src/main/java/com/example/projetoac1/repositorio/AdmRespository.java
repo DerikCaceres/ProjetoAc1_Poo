f8f856f6983e8d993193f4a6831cbc026763e9c0
DerikCaceres <derik.maganhato@hotmail.com>
2021-05-19 18:02:20 -0300

    commit

--- src/main/java/com/example/projetoac1/repositorio/AdmRespository.java
+++ src/main/java/com/example/projetoac1/repositorio/AdmRespository.java
@@ -1,16 +1,27 @@
 package com.example.projetoac1.repositorio;
 
+import com.example.projetoac1.dtoAdm.DtoAdm;
 import com.example.projetoac1.entities.AdminEntity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 
 
 @Repository
-public interface AdmRepository extends JpaRepository <AdminEntity,Long>{
+public interface AdmRespository extends JpaRepository <AdminEntity,Long>{
+    @Query("SELECT c FROM EventsCadastro c " + 
+    "WHERE " +
+    "LOWER(c.nome)           LIKE   LOWER(CONCAT('%', :nome, '%'))          AND " +
+    "LOWER(c.lugar)          LIKE   LOWER(CONCAT('%', :lugar, '%'))         AND " +
+    "LOWER(c.descricao)      LIKE   LOWER(CONCAT('%', :descricao, '%'))" 
+)   
 
+    public Page<DtoAdm>find(PageRequest pageRequest,String name,String telefone,String email);
 }
 
 
--- src/main/java/com/example/projetoac1/service/AdmService.java
+++ src/main/java/com/example/projetoac1/service/AdmService.java
@@ -1,5 +1,79 @@
 package com.example.projetoac1.service;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.example.projetoac1.dtoAdm.DtoAdm;
+import com.example.projetoac1.dtoAdm.DtoAdmInsert;
+import com.example.projetoac1.entities.AdminEntity;
+import com.example.projetoac1.repositorio.AdmRespository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 public class AdmService {
+   
+
+    private AdmRespository repositoryAdmin;
+
+
+    @Autowired
+    private AdmRespository repository;
+
+        public Page<DtoAdm> getAdmin(PageRequest pageRequest,String name,String telefone,String email){
+        
+        Page <DtoAdm> list = repository.find(pageRequest,name,telefone,email);
+        return list.map(adm -> new DtoAdm(adm));
+    } 
+
     
+    public DtoAdm getAdminByCodigo(long id) {
+
+        Optional <AdminEntity> op = repositoryAdmin.findById(id);
+        AdminEntity admin = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado no sistema!!!"));
+
+        return new DtoAdm(admin);
+    }
+
+    
+    public DtoAdm insert(DtoAdmInsert insertDto){
+
+        AdminEntity entity = new AdminEntity(insertDto);
+        entity = repository.save(entity);
+        return new DtoAdm(entity);
+   }
+
+
+
+    public void Remove(Long id){
+        try{
+            repository.deleteById(id);    
+        }
+        catch(EmptyResultDataAccessException e)  {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"ID já apagado ou não encontrado");
+        }
+    }
+
+
+    public DtoAdm atualizar(long id, DtoAdm AdminDtoUp){
+
+        try{
+            AdminEntity admin = repositoryAdmin.getOne(id);
+            admin.setName(AdminDtoUp.getName());
+            admin = repositoryAdmin.save(admin);
+            return new DtoAdm(admin);
+        }catch(EntityNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"ID não encontrado no Sistema!!!");
+        }
+    }
+
+
+    // salvar admin tem que fazer ainda
+ 
+
 }
